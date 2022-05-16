f73dcd54bf0c6607ce99b77aab453350396a270c
yastrebow <yastrebow@gmail.com>
2022-05-16 13:54:40 +0300

    Create project "movies"

---
+++ src/test/java/ru/yastrebov/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yastrebov.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
